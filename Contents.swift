d9ba31723f8bf907b5ad2f29841ed36a1438372b
bindunglife <k007gb@naver.com>
2023-12-01 15:13:41 +0900

    Initial commit of MyPlayground1

---
+++ Contents.swift
@@ -0,0 +1,43 @@
+class Calculator {
+    func add(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+    
+    func subtract(_ a: Double, _ b: Double) -> Double {
+        return a - b
+    }
+    
+    func multiply(_ a: Double, _ b: Double) -> Double {
+        return a * b
+    }
+    
+    func divide(_ a: Double, _ b: Double) -> Double? {
+        guard b != 0 else {
+            print("Error: Division by zero is not allowed.")
+            return nil
+        }
+        return a / b
+    }
+}
+
+// Calculator 클래스 인스턴스 생성
+let calculator = Calculator()
+
+// 더하기 연산
+let additionResult = calculator.add(10.0, 5.0)
+print("더하기 결과: \(additionResult)")
+
+// 빼기 연산
+let subtractionResult = calculator.subtract(10.0, 5.0)
+print("빼기 결과: \(subtractionResult)")
+
+// 곱하기 연산
+let multiplicationResult = calculator.multiply(10.0, 5.0)
+print("곱하기 결과: \(multiplicationResult)")
+
+// 나누기 연산
+if let divisionResult = calculator.divide(10.0, 5.0) {
+    print("나누기 결과: \(divisionResult)")
+}
+
+
